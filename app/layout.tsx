234eb6f1559586ad210496793396199c9aa16d28
Feras Nasr <feras94@protonmail.com>
2024-07-13 13:01:27 +0200

    init commit

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import './globals.css';
+import type { Metadata } from 'next';
+import { IBM_Plex_Sans } from 'next/font/google';
+import { cn } from '../@/lib/utils';
+
+const ibmPlex = IBM_Plex_Sans({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-ibm-plex-sans',
+});
+
+export const metadata: Metadata = {
+  title: 'PixelPulse',
+  description: 'AI-powered image generator',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={cn('font-IBMPlex antialiased', ibmPlex.variable)}>
+        {children}
+      </body>
+    </html>
+  );
+}
